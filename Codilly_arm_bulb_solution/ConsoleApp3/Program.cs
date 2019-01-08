11c08552d31e31260e12a14df2bfb476f75cb4ae
renato87milagres <renato_milagres@yahoo.com.br>
2019-01-08 13:39:39 -0200

    Codility_Solution_Arm_Bulbs

---
+++ Codilly_arm_bulb_solution/ConsoleApp3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System;
+using System.Linq;
+class Solution
+{
+       
+    public int solution1()
+    {
+        int sum = 0;
+        int T = 0;
+        int[] A = new int[] { 2, 3, 4, 1, 5 };
+        for (int i = 0; i < A.Length; i++)
+        {
+            sum = sum + A[i];
+            if (sum >= A.Length)
+            {
+                T = i;
+                i = A.Length;
+            }
+
+        }
+        Console.WriteLine(T);
+        Console.ReadLine();
+        return T;
+
+
+    }
+
+    public int solution2(int[] ranks)
+    {
+        
+        int[] reportToNumber = new int[ranks.Length];
+        int cont = 0;
+
+        for (int i = 0; i < ranks.Length; i++)
+        {
+            reportToNumber[i] = ranks[i] + 1;
+        
+        }
+
+
+        int[] B = ranks.Distinct().ToArray();
+               
+        for (int i = 0; i < B.Length; i++ )
+        {
+            
+            for (int j = 0; j < reportToNumber.Length; j++)
+            {
+              
+
+                if (B[i] == reportToNumber[j])
+                {
+                    cont = cont + 1;
+
+                }
+                
+            }
+
+
+      
+           
+         
+        }
+        Console.WriteLine(cont);
+        Console.ReadKey();
+
+        return cont;
+        
+    }
+
+
+
+
+    static void Main(string[] args)
+    {
+        int[] A = new int[] { 0, 1, 3, 3, 4, 4 };
+        Solution n = new Solution();
+     
+        //int teste = n.solution1();
+        int T = n.solution2(A);
+    }
+
+
+
+}
